00b3128d4384f21942e3529e7b0079513de48b46
harshp30 <72897159+harshp30@users.noreply.github.com>
2020-12-12 11:12:10 -0700

    Update transmitter-glove-code.ino

--- transmitter-glove-code/transmitter-glove-code.ino
+++ transmitter-glove-code/transmitter-glove-code.ino
@@ -1,7 +1,4 @@
-//Mert Arduino and Tech YouTube Channel -- https://goo.gl/ivcZhW
 
-//Add the necessary libraries
-//You can find all the necessary library links in the video description.
 #include <SPI.h>        //SPI library for communicate with the nRF24L01+
 #include "RF24.h"       //The main library of the nRF24L01+
 #include "Wire.h"       //For communicate
@@ -9,12 +6,12 @@
 #include "MPU6050.h"    //The main library of the MPU6050
 
 
-const int MPU_ADDR = 0x68; // I2C address of the MPU-6050. If AD0 pin is set to HIGH, the I2C address will be 0x69.
-int16_t accelerometer_x, accelerometer_y, accelerometer_z; // variables for accelerometer raw data
-int16_t gyro_x, gyro_y, gyro_z; // variables for gyro raw data
-int16_t temperature; // variables for temperature data
-char tmp_str[7]; // temporary variable used in convert function
-char* convert_int16_to_str(int16_t i) { // converts int16 to string. Moreover, resulting strings will have the same length in the debug monitor.
+const int MPU_ADDR = 0x68; 
+int16_t accelerometer_x, accelerometer_y, accelerometer_z;
+int16_t gyro_x, gyro_y, gyro_z;
+int16_t temperature; 
+char tmp_str[7]; 
+char* convert_int16_to_str(int16_t i) { 
 sprintf(tmp_str, "%6d", i);
 return tmp_str;
 }
@@ -27,10 +24,8 @@ int16_t gx, gy, gz;
 //Define packet for the direction (X axis and Y axis)
 int data[2];
 
-//Define object from RF24 library - 9 and 10 are a digital pin numbers to which signals CE and CSN are connected.
 RF24 radio(9,10);
-
-//Create a pipe addresses for the communicate                                    
+                                   
 const uint64_t pipe = 0xE8E8F0F0E1LL;
 
 void setup(void){
@@ -43,22 +38,22 @@ void setup(void){
 
 void loop(void){
   Wire.beginTransmission(MPU_ADDR);
-  Wire.write(0x3B); // starting with register 0x3B (ACCEL_XOUT_H) [MPU-6000 and MPU-6050 Register Map and Descriptions Revision 4.2, p.40]
-  Wire.endTransmission(false); // the parameter indicates that the Arduino will send a restart. As a result, the connection is kept active.
-  Wire.requestFrom(MPU_ADDR, 7*2, true); // request a total of 7*2=14 registers
-  // "Wire.read()<<8 | Wire.read();" means two registers are read and stored in the same variable
-  accelerometer_x = Wire.read()<<8 | Wire.read(); // reading registers: 0x3B (ACCEL_XOUT_H) and 0x3C (ACCEL_XOUT_L)
-  accelerometer_y = Wire.read()<<8 | Wire.read(); // reading registers: 0x3D (ACCEL_YOUT_H) and 0x3E (ACCEL_YOUT_L)
-  accelerometer_z = Wire.read()<<8 | Wire.read(); // reading registers: 0x3F (ACCEL_ZOUT_H) and 0x40 (ACCEL_ZOUT_L)
-  temperature = Wire.read()<<8 | Wire.read(); // reading registers: 0x41 (TEMP_OUT_H) and 0x42 (TEMP_OUT_L)
-  gyro_x = Wire.read()<<8 | Wire.read(); // reading registers: 0x43 (GYRO_XOUT_H) and 0x44 (GYRO_XOUT_L)
-  gyro_y = Wire.read()<<8 | Wire.read(); // reading registers: 0x45 (GYRO_YOUT_H) and 0x46 (GYRO_YOUT_L)
-  gyro_z = Wire.read()<<8 | Wire.read(); // reading registers: 0x47 (GYRO_ZOUT_H) and 0x48 (GYRO_ZOUT_L)
+  Wire.write(0x3B); 
+  Wire.endTransmission(false); 
+  Wire.requestFrom(MPU_ADDR, 7*2, true); 
+
+  accelerometer_x = Wire.read()<<8 | Wire.read(); 
+  accelerometer_y = Wire.read()<<8 | Wire.read(); 
+  accelerometer_z = Wire.read()<<8 | Wire.read();
+  temperature = Wire.read()<<8 | Wire.read(); 
+  gyro_x = Wire.read()<<8 | Wire.read(); 
+  gyro_y = Wire.read()<<8 | Wire.read(); 
+  gyro_z = Wire.read()<<8 | Wire.read(); 
   // print out data
   Serial.print("aX = "); Serial.print(convert_int16_to_str(accelerometer_x));
   Serial.print(" | aY = "); Serial.print(convert_int16_to_str(accelerometer_y));
   Serial.print(" | aZ = "); Serial.print(convert_int16_to_str(accelerometer_z));
-  // the following equation was taken from the documentation [MPU-6000/MPU-6050 Register Map and Description, p.30]
+
   Serial.print(" | tmp = "); Serial.print(temperature/340.00+36.53);
   Serial.print(" | gX = "); Serial.print(convert_int16_to_str(gyro_x));
   Serial.print(" | gY = "); Serial.print(convert_int16_to_str(gyro_y));
@@ -67,12 +62,8 @@ void loop(void){
   // delay
   delay(1000);
   
-  //With this function, the acceleration and gyro values of the axes are taken. 
-  //If you want to control the car axis differently, you can change the axis name in the map command.
   mpu.getMotion6(&ax, &ay, &az, &gx, &gy, &gz);
 
-  //In two-way control, the X axis (data [0]) of the MPU6050 allows the robot to move forward and backward. 
-  //Y axis (data [0]) allows the robot to right and left turn.
   data[0] = map(ax, -17000, 17000, 300, 400 ); //Send X axis data
   data[1] = map(ay, -17000, 17000, 100, 200);  //Send Y axis data
   radio.write(data, sizeof(data));
